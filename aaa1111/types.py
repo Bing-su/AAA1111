bfa21904a112f21a1d3348d67d9dcdcb15e30d74
Bingsu <ks2515@naver.com>
2023-07-13 18:34:37 +0900

    feat: utils, types

---
+++ aaa1111/types.py
@@ -0,0 +1,109 @@
+from dataclasses import dataclass, field
+from enum import IntEnum
+from pathlib import Path
+from typing import Any, Dict, List, Literal, Mapping, Optional, Union
+
+from beartype import beartype
+from PIL import Image
+
+Number = Union[int, float]
+
+
+@beartype
+@dataclass
+class Common:
+    prompt: str = ""
+    styles: List[str] = field(default_factory=list)
+    seed: int = -1
+    subseed: int = -1
+    subseed_strength: Number = 0.0
+    seed_resize_from_h: int = -1
+    seed_resize_from_w: int = -1
+    sampler_name: Optional[str] = None  # the first available sampler
+    batch_size: int = 1
+    n_iter: int = 1
+    steps: int = 50
+    cfg_scale: Number = 7.0
+    width: int = 512
+    height: int = 512
+    restore_faces: bool = False
+    tiling: bool = False
+    do_not_save_samples: bool = False
+    do_not_save_grid: bool = False
+    negative_prompt: str = ""
+    eta: Optional[Number] = None
+    s_min_uncond: Number = 0.0
+    s_churn: Number = 0.0
+    s_tmax: Optional[Number] = None
+    s_tmin: Number = 0.0
+    s_noise: Number = 1.0
+    override_settings: Optional[Mapping[str, Any]] = None
+    override_settings_restore_afterwards: bool = True
+    script_args: List[Any] = field(default_factory=list)
+    script_name: Optional[str] = None
+    send_images: bool = True
+    save_images: bool = False
+    alwayson_scripts: Mapping[str, Any] = field(default_factory=dict)
+
+
+@beartype
+@dataclass
+class TXT2IMG(Common):
+    enable_hr: bool = False
+    denoising_strength: Number = 0.0
+    firstphase_width: int = 0
+    firstphase_height: int = 0
+    hr_scale: Number = 2.0
+    hr_upscaler: Optional[str] = None  # Latent (nearest)
+    hr_second_pass_steps: int = 0
+    hr_resize_x: int = 0
+    hr_resize_y: int = 0
+    hr_sampler_name: Optional[str] = None  # same as sampler_name
+    hr_prompt: str = ""
+    hr_negative_prompt: str = ""
+
+
+class ResizeMode(IntEnum):
+    Just_resize = 0
+    Crop_and_resize = 1
+    Resize_and_fill = 2
+    Just_resize_latent_upscale = 3
+
+
+class InpaintingFill(IntEnum):
+    Fill = 0
+    Original = 1
+    Latent_noise = 2
+    Latent_nothing = 3
+
+
+class InpaintingMaskInvert(IntEnum):
+    Inpaint_masked = 0
+    Inpaint_not_masked = 1
+
+
+@beartype
+@dataclass
+class IMG2IMG(Common):
+    init_images: List[Union[str, Path, Image.Image]] = field(default_factory=list)
+    resize_mode: Union[ResizeMode, Literal[0, 1, 2, 3]] = ResizeMode.Just_resize
+    denoising_strength: Number = 0.75
+    image_cfg_scale: Optional[Number] = None
+    mask: Union[str, Path, Image.Image, None] = None
+    mask_blur: Optional[int] = None
+    mask_blur_x: int = 4
+    mask_blur_y: int = 4
+    inpainting_fill: Union[InpaintingFill, Literal[0, 1, 2, 3]] = InpaintingFill.Fill
+    inpaint_full_res: bool = True
+    inpaint_full_res_padding: int = 0
+    inpainting_mask_invert: Union[
+        InpaintingMaskInvert, Literal[0, 1]
+    ] = InpaintingMaskInvert.Inpaint_masked
+    initial_noise_multiplier: Optional[Number] = None
+
+
+@dataclass
+class Response:
+    images: List[Image.Image]
+    parameters: Dict[str, Any]
+    info: Dict[str, Any]
---
+++ aaa1111/utils.py
@@ -0,0 +1,118 @@
+import base64
+import io
+from pathlib import Path
+from typing import Any, Dict, Mapping, Sequence, Union
+
+import filetype
+import orjson
+import rtoml
+from aiofile import async_open
+from asyncer import asyncify
+from PIL import Image
+from ruamel.yaml import YAML
+
+Image_Type = Union[str, Path, Image.Image]
+DICT_EXT = (".toml", ".yaml", ".yml", "json")
+
+
+def image_to_base64(img: Image_Type) -> str:
+    if isinstance(img, Image.Image):
+        buffer = io.BytesIO()
+        img.save(buffer)
+        value = buffer.getvalue()
+    else:
+        with open(img, "rb") as f:
+            value = f.read()
+    return base64.b64encode(value).decode("utf-8")
+
+
+async def aimage_to_base64(img: Image_Type) -> str:
+    if isinstance(img, Image.Image):
+        buffer = io.BytesIO()
+        img.save(buffer)
+        value = buffer.getvalue()
+    else:
+        async with async_open(img, "rb") as f:
+            value = await f.read()
+    return base64.b64encode(value).decode("utf-8")
+
+
+def base64_to_image(s: str) -> Image.Image:
+    return Image.open(io.BytesIO(base64.b64decode(s)))
+
+
+abase64_to_image = asyncify(base64_to_image)
+
+
+def is_image_file(obj: Any) -> bool:
+    if isinstance(obj, (str, Path)) and Path(obj).is_file():
+        return filetype.image_match(obj) is not None
+    return False
+
+
+def recursive_read_image(item: Any):
+    if isinstance(item, Sequence):
+        return [
+            image_to_base64(v) if is_image_file(v) else recursive_read_image(v)
+            for v in item
+        ]
+    if isinstance(item, Mapping):
+        return {
+            k: image_to_base64(v) if is_image_file(v) else recursive_read_image(v)
+            for k, v in item.items()
+        }
+    return item
+
+
+async def arecursive_read_image(item: Union[Sequence[Any], Mapping[str, Any]]):
+    if isinstance(item, Sequence):
+        return [
+            (await aimage_to_base64(v))
+            if is_image_file(v)
+            else (await arecursive_read_image(v))
+            for v in item
+        ]
+    if isinstance(item, Mapping):
+        return {
+            k: (await aimage_to_base64(v))
+            if is_image_file(v)
+            else (await arecursive_read_image(v))
+            for k, v in item.items()
+        }
+    return item
+
+
+def is_valid_dict_file(file: Union[str, Path]) -> bool:
+    ext = Path(file).suffix
+    return ext in DICT_EXT
+
+
+def load_dict_file(file: Union[str, Path]) -> Dict[str, Any]:
+    if not is_valid_dict_file(file):
+        msg = f"Unsupported file extension: {file!r}"
+        raise ValueError(msg)
+    ext = Path(file).suffix
+
+    with open(file, encoding="utf-8") as raw:
+        if ext == ".toml":
+            return rtoml.load(raw)
+        if ext == ".json":
+            return orjson.loads(raw.read())
+        yaml = YAML()
+        return dict(yaml.load(raw))
+
+
+async def aload_dict_file(file: Union[str, Path]) -> Dict[str, Any]:
+    if not is_valid_dict_file(file):
+        msg = f"Unsupported file extension: {file!r}"
+        raise ValueError(msg)
+    ext = Path(file).suffix
+
+    async with async_open(file, encoding="utf-8") as raw:
+        data = await raw.read()
+        if ext == ".toml":
+            return rtoml.loads(data)
+        if ext == ".json":
+            return orjson.loads(data)
+        yaml = YAML()
+        return dict(yaml.load(data))
