d925f8146a84c08c9bedddac0cc5337d047050de
Bingsu <ks2515@naver.com>
2023-07-14 11:38:24 +0900

    feat: cli output

--- aaa1111/__main__.py
+++ aaa1111/__main__.py
@@ -1,7 +1,11 @@
+import io
 from pathlib import Path
-from typing import List, Optional
+from typing import Any, Dict, List, Optional
 
-from rich.status import Status
+import rich.progress as pg
+from rich.panel import Panel
+from rich.syntax import Syntax
+from ruamel.yaml import YAML
 from typer import Argument, Option, Typer
 from typing_extensions import Annotated
 from ulid import ULID
@@ -11,6 +15,7 @@ from aaa1111.utils import load_dict_file
 
 app_txt2img = Typer()
 app_img2img = Typer()
+yaml = YAML()
 
 
 @app_txt2img.command(no_args_is_help=True)
@@ -52,21 +57,7 @@ def txt2img(
     https: bool = False,
     save_ext: str = "png",
 ):
-    client = AAA1111(host=host, port=port, base_url=base_url, https=https)
-    output.mkdir(parents=True, exist_ok=True)
-    length = len(params)
-
-    for i in range(length):
-        payload = load_dict_file(params[i])
-        prompts = payload.get("prompt", "")
-        negatives = payload.get("negative_prompt", "")
-        with Status(
-            f"Generating task {i + 1}/{length}...\nprompts: {prompts}\nnegatives: {negatives}"
-        ):
-            resp = client.txt2img(payload)
-
-            for image in resp.images:
-                image.save(output / (f"{ULID()}.{save_ext}"), lossless=True)
+    _inner(params, output, base_url, host, port, https, save_ext, task="txt2img")
 
 
 @app_img2img.command(no_args_is_help=True)
@@ -108,21 +99,56 @@ def img2img(
     https: bool = False,
     save_ext: str = "png",
 ):
+    _inner(params, output, base_url, host, port, https, save_ext, task="img2img")
+
+
+def _inner(
+    params: List[Path],
+    output: Path,
+    base_url: Optional[str],
+    host: str,
+    port: int,
+    https: bool,
+    save_ext: str,
+    *,
+    task: str = "txt2img",
+):
     client = AAA1111(host=host, port=port, base_url=base_url, https=https)
     output.mkdir(parents=True, exist_ok=True)
     length = len(params)
 
-    for i in range(length):
-        payload = load_dict_file(params[i])
-        prompts = payload.get("prompt", "")
-        negatives = payload.get("negative_prompt", "")
-        with Status(
-            f"Generating task {i + 1}/{length}...\nprompts: {prompts}\nnegatives: {negatives}"
-        ):
-            resp = client.img2img(payload)
+    progress = pg.Progress(
+        pg.SpinnerColumn(),
+        pg.TextColumn("[progress.description]{task.description}"),
+        pg.TimeElapsedColumn(),
+        pg.TimeRemainingColumn(),
+        pg.MofNCompleteColumn(),
+    )
+
+    with progress:
+        for i in progress.track(range(length), description=f"Processing {task}..."):
+            payload = load_dict_file(params[i])
+            panel = Panel(
+                Syntax(format_payload(payload), "yaml", theme="ansi_dark"),
+                title=f"{task} [green]{i + 1}/{length}[/green]",
+            )
+            progress.print(panel)
+            if task == "txt2img":
+                resp = client.txt2img(payload)
+            elif task == "img2img":
+                resp = client.img2img(payload)
+            else:
+                msg = f"Unknown task: {task}"
+                raise ValueError(msg)
 
             for image in resp.images:
                 image.save(output / (f"{ULID()}.{save_ext}"), lossless=True)
+
+
+def format_payload(payload: Dict[str, Any]) -> str:
+    stream = io.StringIO()
+    yaml.dump(payload, stream)
+    return stream.getvalue().strip()
 
 
 if __name__ == "__main__":
